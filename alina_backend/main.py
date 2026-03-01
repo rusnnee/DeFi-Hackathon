07ce9e49f2526e95e19dd81d8cfe835cf590f53b
rusnnee <rusne.jon@gmail.com>
2026-03-01 10:14:51 +0000

    Add new allowed origin for CORS middleware

--- alina_backend/main.py
+++ alina_backend/main.py
@@ -12,7 +12,11 @@ import time
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:30...",
+    "https://de-fi-hackathon.vercel.app"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
